a3417bceb39220809ccf8f24ea4295ea91e94172
DuduBertolDev <88258208+DuduBertol@users.noreply.github.com>
2023-06-29 21:12:55 -0300

    Calendar finish and Routine begins

--- HackaNav/Assets/Scripts/SlidePages.cs
+++ HackaNav/Assets/Scripts/SlidePages.cs
@@ -10,6 +10,7 @@ public class SlidePages : MonoBehaviour
     public List<float> pageLocationX = new List<float>(); // move-se a PageController
     public List<float> pageLocationY = new List<float>(); // move-se individualmente cada página
     public List<GameObject> scrollPages = new List<GameObject>();
+    public List<GameObject> scrollbarList = new List<GameObject>();
     public int pageID;
     //public List<bool> buttonClick = new List<bool>();
 
@@ -41,8 +42,13 @@ public class SlidePages : MonoBehaviour
         {
             if(pageID == i)
             {
+                scrollbarList[i].gameObject.LeanScale(new Vector3(1,1,1), 0.1f);
                 LeanTween.moveX(pageController.GetComponent<RectTransform>(), pageLocationX[i], 0.5f).setEaseInOutExpo();
                 LeanTween.moveY(scrollPages[i].GetComponent<RectTransform>(), pageLocationY[i], 0.5f).setEaseInOutExpo(); 
+            }
+            else
+            {
+                scrollbarList[i].gameObject.LeanScale(new Vector3(0,0,0), 0.1f);
             }
         }
     }
